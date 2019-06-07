f560c1d844e3f7450a9cc8679267eb42da520ff4
jiangyuanluo <35863308+jiangyuanluo@users.noreply.github.com>
2019-06-07 10:36:09 -0700

    Update wrapper_breakseq2.sh

--- Breakseq/wrapper_breakseq2.sh
+++ Breakseq/wrapper_breakseq2.sh
@@ -1,19 +1,18 @@
 #!/bin/bash
 
-. /u/local/Modules/default/init/modules.sh
-module load samtools
-module load python/2.7
-
 input=$1
 outdir=$2
 reference=$3
 filename=$(basename $input)
-dir='/u/home/j/jiangyua/breakseq2-2.2'
+
+# Directory where breakseq is installed
+dir='/home/jluo/breakseq2-2.2'
 toolName="BreakSeq(v2.2)"
 mkdir $outdir
 
 # -----------------------------------------------
 
+# Start time and log
 now="$(date)"
 
 logfile=${outdir}/report_breakseq_v2.2.${filename}.log
@@ -23,15 +22,25 @@ printf "%s --- RUNNING %s\n" "$now" $toolName >> $logfile
 
 res1=$(date +%s.%N)
 
+# Create a work directory for the file for all outputs
 work=${outdir}/${filename}
+
+# Name the output vcf file
 vcf=${outdir}/${filename}/${filename}.vcf
 
+# SV calling
+# python is required
+# Have to specify the location of bwa and samtools
+# breakpoint library for specific reference genome is required
 python $dir/scripts/run_breakseq2.py --reference $reference --bams $input --work $work --bwa /u/local/apps/bwa/0.7.17/gcc-4.4.7/bwa --samtools /u/local/apps/samtools/0.1.19/gcc-4.4.7/samtools --bplib /u/home/j/jiangyua/breakseq2_bplib_20150129.hg37/breakseq2_bplib_20150129.fna
 
+# Unzip the folder
 gunzip $work/breakseq.vcf.gz
 
+# Extract the vcf file from unzipped file
 mv $work/breakseq.vcf $vcf
 
+# End time
 res2=$(date +%s.%N)
 dt=$(echo "$res2-$res1" | bc)
 dd=$(echo "$dt/86400" | bc)
