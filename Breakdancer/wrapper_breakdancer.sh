549744a708661857330592fd6f2cace0c6c58790
jiangyuanluo <35863308+jiangyuanluo@users.noreply.github.com>
2019-06-07 09:08:36 -0700

    Update wrapper_breakdancer.sh

--- Breakdancer/wrapper_breakdancer.sh
+++ Breakdancer/wrapper_breakdancer.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
 
+# Bam file
 input=$1
+
+# Output directory
 outdir=$2
+
 filename=$(basename $input)
+
+# The directory containing breakdancer installed
 dir='/home/jluo/breakdancer'
 toolName="BreakDancer(v1.4.5)"
 mkdir $outdir
 
 # -----------------------------------------------
 
+# Start time and log
 now="$(date)"
 
 logfile=${outdir}/report_breakdancer_v1.4.5.${filename}.log
@@ -18,14 +25,21 @@ printf "%s --- RUNNING %s\n" "$now" $toolName >> $logfile
 
 res1=$(date +%s.%N)
 
+# Name the output files
 cfg=${outdir}/${filename}.cfg
 call=${outdir}/${filename}.calls
 vcf=${outdir}/${filename}.vcf
 
+# Convert the original bam file to configuration file
 perl $dir/perl/bam2cfg.pl $input > $cfg
+
+# SV calling
 $dir/build/bin/breakdancer-max $cfg > $call
+
+# Convert the call file to vcf file
 python $dir/tools/generateVCF.py $call
 
+# End time
 res2=$(date +%s.%N)
 dt=$(echo "$res2-$res1" | bc)
 dd=$(echo "$dt/86400" | bc)
